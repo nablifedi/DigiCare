a99d1a0b7ab573da18c1adbd5ac843dd17d43c96
azizhajlaoui <93492402+azizhajlaoui@users.noreply.github.com>
2021-12-14 20:19:49 +0100

    hajlaoui

---
+++ backend/data/staff.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const staffSchema = new Schema({
+ staff:{
+    joined: Date ,
+    education: String,
+    certificate: String,
+    language: String,
+  }
+ 
+})
+
+const staff = mongoose.model("staff", userSchema);
+
+export default staff;
