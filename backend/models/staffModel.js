8ae42b41da5b5dbeea99da83a4bdbb2a6dc725ac
nablifedi <fedinabli@ieee.org>
2021-12-14 22:42:29 +0100

    Updated staff

--- backend/models/staffModel.js
+++ backend/models/staffModel.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 
 const staffSchema = mongoose.Schema({
+  person: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Person'
+  },
   joined: {
     type: Date,
     required: true
